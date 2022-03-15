3a82867f2de0b66a6fa8d55d78dafe569175b674
MOHIT PERIWAL <84000709+MohitP-2002@users.noreply.github.com>
2022-03-15 08:10:28 +0530

    Add files via upload

---
+++ pages/store/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+import ProductsGrid from './ProductsGrid';
+
+const Store = () => {
+
+    return (
+        <Layout title="Store" description="This is the Store page" >
+            <div >
+                <div className="text-center mt-5">
+                    <h1>Store</h1>
+                </div>
+                <ProductsGrid />
+            </div>
+        </Layout>
+    );
+}
+
+export default Store;
